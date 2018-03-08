ace56c403a37482c29f72f2578b77c52ff6bf445
wuhong_home <444089024@qq.com>
2018-03-09 00:43:00 +0800

    #home add commit

--- yx-common/src/main/java/com/myright/infra/common/DateUtil.java
+++ yx-common/src/main/java/com/myright/infra/common/DateUtil.java
@@ -14,6 +14,12 @@ public class DateUtil {
         return formatDate(new Date(), pattern);
     }
 
+    /**
+     * date to string
+     * @param date
+     * @param pattern
+     * @return
+     */
     public static String formatDate(Date date, String pattern){
         return new SimpleDateFormat(pattern).format(date);
     }
